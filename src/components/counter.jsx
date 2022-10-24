a8bf560ac9b1ab70644232239178bed90c6cf000
Narayan Aryal <aryal.coding@gmail.com>
2022-10-24 12:16:15 +1100

    More code added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,30 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-
+    
+      
+    
     };
+    handleOnIncrement() {
+        console.log("Clicked");
+        };
     render() { 
+
+        let classes = this.getBgClasses();
+
         return (
         <div>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className= {this.getBgClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleOnIncrement} className='btn btn-success'>Increment</button>
         </div>
         );
     }
+    getBgClasses() {
+        let classes = "bg m-2 rounded bg-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
    formatCount () {
         const{count} = this.state;
         return count === 0 ? 'zero' : count;
